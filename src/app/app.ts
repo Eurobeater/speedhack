ed7449f430a979f731b938b2d8b54b0de41e836f
Eurobeater <antoniogallegopenalver@gmail.com>
2025-08-07 18:19:23 +0200

    Fuentes añadidas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './componentes/home/home';
+import { Navbar } from "./componentes/navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
